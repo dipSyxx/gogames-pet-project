90cbbfe4f65e6e14196ea7bcaaa2c64709c111da
Yura <linetsky.yura@gmail.com>
2023-04-30 13:14:03 +0200

    finished create burger menu

--- src/modules/BurgerMenu/BurgerMenu.tsx
+++ src/modules/BurgerMenu/BurgerMenu.tsx
@@ -2,6 +2,8 @@ import clsx from 'clsx'
 import React from 'react'
 
 import styles from './BurgerMenuStyle.module.sass'
+import { HeaderLinks, HeaderLinksProps } from '@/data/HeaderLinks'
+import { Link } from 'react-scroll'
 
 type BurgerMenuProps = {
   openBurgerMenu: boolean
@@ -28,12 +30,32 @@ export const BurgerMenu = ({ openBurgerMenu, setOpenBurgerMenu, active, setActiv
       <div className={clsx(styles.burger_window, openBurgerMenu ? styles.burger_window_show : '')}>
         <div className={styles.burger_menu_header}>
           {/* header */}
+          <img src="/logo/logoHead.png" alt="logo" />
+          <div className={styles.header_text}>Menu</div>
           <button className={styles.burger_close} onClick={handleCloseBurgerMenu}>
             X
           </button>
         </div>
 
-        <div className={styles.burger_menu_body}></div>
+        <div className={styles.burger_menu_body}>
+          <ul className={styles.menu_links}>
+            {HeaderLinks.map(({ id, name, href }: HeaderLinksProps) => (
+              <li key={id} className={clsx(styles.link_item)}>
+                <Link
+                  onClick={handleCloseBurgerMenu}
+                  activeClass={styles.activeLink}
+                  to={href}
+                  spy={true}
+                  smooth={true}
+                  offset={-90}
+                  duration={600}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       {/* Контентна частина ================== */}
     </>
